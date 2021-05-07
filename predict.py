b6cad63ad30d7528b6424ae023167081bcbee4df
Berniece Buttermilk <bbutterm@mi-e5.msk.21-school.ru>
2021-05-07 19:22:32 +0300

    123

---
+++ predict.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import sys
+from linear.theta import *
+
+def main():
+	if (len(sys.argv) == 1):
+		km = input("Enter mileage: ")
+		try:
+			val = int(km)
+			if (val < 0):
+				print("That's not a positive int!")
+				return
+		except ValueError:
+			print("That's not an int!")
+			return
+		result = (theta0 + (theta1 * int(km)))
+		print ("Result: ", result)
+
+	elif (len(sys.argv) > 1):
+		print ("Too few arguments")
+
+if __name__ == '__main__':
+	main()
---
+++ train.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+import sys, re
+
+def createNewFile(theta0, theta1):
+    try:
+        file = open("theta.py", 'w+')
+        file.write("theta0 = ")
+        file.write(str(theta0))
+        file.write("\n")
+        file.write("theta1 = ")
+        file.write(str(theta1))
+        file.write("\n")
+        file.close()
+    except:
+        print('Something went wrong! Can\'t tell what?')
+        sys.exit(0)
+    return
+
+def calculLinearRegression(lstLeft, lstRight, lstPattern):
+    sum0, sum1 = 0, 0
+    tmpTheta0, tmpTheta1 = 0, 0
+    learningRate, i = 0.1, 0
+    # miss other loop
+    while (i < len(lstLeft)):
+        sum0 = (tmpTheta0 + (tmpTheta1 * lstLeft[i])) - lstRight[i]
+        sum1 = ((tmpTheta0 + (tmpTheta1 * lstLeft[i])) - lstRight[i]) * lstLeft[i]
+        tmpTheta0 -= learningRate * sum0
+        tmpTheta1 -= learningRate * sum1
+        i += 1
+    i = 0
+    print ("tmpTheta0: ", tmpTheta0)
+    print ("tmpTheta1: ", tmpTheta1)
+    print ("sum0", sum0)
+    createNewFile(tmpTheta0, tmpTheta1)
+    return
+
+def createList(arg):
+    lstLeft = []
+    lstRight = []
+    lstPattern = []
+    file_object = open(arg, "r")
+    lstObj = list(file_object)
+
+    for line in lstObj:
+        if (lstObj[0] == line):
+            if (line.find(",") == -1):
+                print ("Error line: ", line)
+                exit(0)
+            else:
+                line = line[:line.find("\n")]
+                split = line.split(',')
+                if (len(split) == 2):
+                    lstPattern.append(split[0])
+                    lstPattern.append(split[1])
+                else:
+                    print ("Error line: ", line)
+                    exit(0)
+        else:
+            if (re.search('^(\\d+),(\\d+)$', line) is None):
+                print ("Error line: ", line)
+                exit(0)
+            match = re.match('^(\\d+),(\\d+)$', line)
+            lstLeft.append(int(match.group(1)))
+            lstRight.append(int(match.group(2)))
+    return [lstLeft, lstRight, lstPattern]
+
+def main():
+    if (len(sys.argv) == 2):
+        lst = createList(sys.argv[1])
+        calculLinearRegression(lst[0], lst[1], lst[2])
+        print (lst)
+    elif (len(sys.argv) > 2):
+        print ("Too few arguments")
+    else:
+        print ("Put one argument")
+
+if __name__ == '__main__':
+    main()
